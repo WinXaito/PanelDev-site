64168c94d413e869c7b4a785e182b1dbdcae1c4e
WinXaito <github@winlap.ch>
2017-02-01 22:28:20 +0100

    Replace bootstrap.js and chart.js by CDN

--- views/templates_pages/home/js_home.php
+++ views/templates_pages/home/js_home.php
@@ -6,7 +6,7 @@
 	 */
 
 	return '
-		<script src="assets/libraries/js/chart.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 
 		<script>
 			var config = {
