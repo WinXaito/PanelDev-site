7c65ab195c696d5087e230501f6c22a7ecc049ef
WinXaito <github@winlap.ch>
2017-02-02 20:31:45 +0100

    Remove acces to class and templates

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
-	/**
-	 * Project: PanelDev
-	 * License: GPL3.0 ©All right reserved
-	 * User: WinXaito
-	 */
+/**
+ * Project: PanelDev
+ * License: GPL3.0 ©All right reserved
+ * User: WinXaito
+ */
 
-	//Generale
-		define('PATH', __DIR__);
-        define('DEBUG', true);
-        define('TWIG_CACHE', false);
+//Generale
+    define('PATH', __DIR__);
+    define('DEBUG', true);
+    define('TWIG_CACHE', false);
 
-	//Url
-		define('URL_PATH', "/winxaito/paneldev");
-		define('URL_PATH_HOME', "/winxaito/paneldev/");
-		define('URL_PROJECTS_LENGTH', 6);
+//Url
+    define('URL_PATH', "/winxaito/paneldev");
+    define('URL_PATH_HOME', "/winxaito/paneldev/");
+    define('URL_PROJECTS_LENGTH', 6);
 
-	//BDD
-		define('DB_HOST', 'localhost');
-		define('DB_NAME', 'paneldev');
-		define('DB_USERNAME', 'root');
-		define('DB_PASSWORD', '');
+//BDD
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'paneldev');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
