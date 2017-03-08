e9a67f9a2b92c22d621d7154c6c047d972371e33
WinXaito <github@winlap.ch>
2017-03-08 19:37:10 +0100

    Limit number projects and separe in groups

--- controllers/projects/projects.php
+++ controllers/projects/projects.php
@@ -11,20 +11,34 @@ Wx_Session::requireAuthentication();
 
 $tab['projects'] = "active";
 
+$TYPE = ['website' => 'Site web', 'general' => 'Général', 'print3d' => 'Impression 3D'];
 $projects = Wx_ProjectManager::getAllUserProjects(Wx_Session::getUser());
+$type = isset($_GET['type']) && !empty($_GET['type']) && array_key_exists($_GET['type'], $TYPE) ? $_GET['type'] : 'all';
 
-$breadcrum = new Wx_Breadcrum(
-    false,
-    [
-        'Accueil' => '',
-        'Projets' => 'projects'
-    ]
-);
+if($type == 'all') {
+    $breadcrum = new Wx_Breadcrum(
+        false,
+        [
+            'Accueil' => '',
+            'Projets' => 'projects',
+        ]
+    );
+}else{
+    $breadcrum = new Wx_Breadcrum(
+        false,
+        [
+            'Accueil' => '',
+            'Projets' => 'projects',
+            $TYPE[$type] => $type,
+        ]
+    );
+}
 
 echo $twig->render('templates_pages/projects/content_projects.twig', [
     'tab' => $tab,
     'breadcrum' => $breadcrum->getBreadcrum(),
     'projects' => $projects,
+    'type' => $type,
 ]);
 
 Wx_Utils::showDebugInfos();
